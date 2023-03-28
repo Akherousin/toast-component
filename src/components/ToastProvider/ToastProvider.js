edb15121e404060f58e8d75085df7a2ace085690
Arthur Badin <akherousin94@gmail.com>
2023-03-29 02:47:01 +0600

    Add keyboard event listener to dismiss all toasts when Escape key is pressed in ToastProvider

--- src/components/ToastProvider/ToastProvider.js
+++ src/components/ToastProvider/ToastProvider.js
@@ -5,6 +5,18 @@ export const ToastContext = React.createContext();
 function ToastProvider({ children }) {
   const [toasts, setToasts] = React.useState([]);
 
+  React.useEffect(() => {
+    const dismissAllToasts = (e) => {
+      if (e.code === 'Escape') {
+        setToasts([]);
+      }
+    };
+
+    window.addEventListener('keydown', dismissAllToasts);
+
+    return window.removeEventListener('keydown', dismissAllToasts);
+  }, []);
+
   const addNewToast = React.useCallback(
     (variant, text) => {
       const newToast = { variant, text, id: crypto.randomUUID() };
